869b73745b776f803d2674015d23b868fe247a51
Anna <a.dimitrova.au@gmail.com>
2017-04-24 14:30:53 +1000

    added dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user! , only: [:dashboard]
   def home
     if current_user
       redirect_to medications_path
     end
+  end
+
+  def dashboard
+    @medications = current_user.medications
+
   end
 end
