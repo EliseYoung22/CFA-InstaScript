647073713a8d786094e97f11100a754a0b212532
Elise Young <eliseyoung_4@hotmail.com>
2017-04-25 18:02:08 +1000

    change indentation

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -4,7 +4,7 @@ class Medication < ApplicationRecord
 
   belongs_to :user
   has_many :sales
-  
+
   has_attached_file :image
   has_attached_file :resource
 
@@ -13,14 +13,14 @@ class Medication < ApplicationRecord
   message: 'Only images allowed'
 
   validates_attachment :resource, presence: true,
-  content_type: { content_type: "application/pdf" }, 
+  content_type: { content_type: "application/pdf" },
   message: 'Only PDF allowed'
 
   validates_numericality_of :price,
   greater_than: 49, message: "Price must be at least 50 cents"
 
   scope(:medication, -> (name) { where("name like ?", "#{name}%")})
-scope(:description, -> (description) { where("description like ?", "%#{description}%")})
-scope(:price, -> (price) { where price: price })
+  scope(:description, -> (description) { where("description like ?", "%#{description}%")})
+  scope(:price, -> (price) { where price: price })
 
 end
